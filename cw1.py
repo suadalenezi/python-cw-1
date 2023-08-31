730f15a92888c4650ad5f365526c69f610299b7b
suadalenezi <140262573+suadalenezi@users.noreply.github.com>
2023-08-31 15:10:55 +0300

    Create cw1.py

---
+++ cw1.py
@@ -0,0 +1,9 @@
+print("suad")
+print(4+4)
+print(4/4)
+print(4*4)
+length = 7
+width = 8
+area = length * width 
+print(f"the result is area {area}")
+print(f"If the length of the rectangle is {length}, and width is {width}, then the area is {area}")
